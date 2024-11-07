4da4efaf69f75ef8cf34b12e4ca4194477257394
andreidanielpopa <andreidanielpopa02@gmail.com>
2024-11-07 09:31:56 +0100

    finalizado

--- src/app/components/tablamultiplicar/tablamultiplicar.component.ts
+++ src/app/components/tablamultiplicar/tablamultiplicar.component.ts
@@ -1,25 +1,30 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-tablamultiplicar',
-  templateUrl: './tablamultiplicar.component.html',
-  styleUrl: './tablamultiplicar.component.css'
+    selector: 'app-tablamultiplicar',
+    templateUrl: './tablamultiplicar.component.html',
+    styleUrl: './tablamultiplicar.component.css'
 })
 export class TablamultiplicarComponent {
 
-  @ViewChild('cajanumero') cajaNumeroRef: ElementRef;
+    @ViewChild('cajanumero') cajaNumeroRef!: ElementRef;
 
-  public num: number;
-  public index: Array<number>;
+    public num: number;
+    public index: Array<number>;
 
-  constructor() {
-    this.num = 0;
-    this.index = new Array<number>();
-    this.cajaNumeroRef = new ElementRef(0);
-  }
+    constructor() {
+        this.num = 0;
+        this.index = new Array<number>();
+    }
 
-  multiplicar(): void {
-    this.num = this.cajaNumeroRef.nativeElement.value;
-  }
+    multiplicar(): void {
+        let aux = new Array<number>();
+        this.num = this.cajaNumeroRef.nativeElement.value;
+        for (var i = 1; i <= 10; i++) {
+            var resultado = this.num * i;
+            aux.push(resultado);
+        }
+        this.index = aux;
+    }
 
 }
